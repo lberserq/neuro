c2447a6f2a11f5cdd4b335fcc947e03531378fe5
Polandr <fla_444@mail.ru>
2015-02-28 17:05:59 +0300

    Update and rename src/parsers to src/parsers/edfparser.h

---
+++ src/parsers/edfparser.h
@@ -0,0 +1,70 @@
+#ifndef EDFPARSER_H
+#define EDFPARSER_H
+
+#include "edflib.h"
+
+#include <vector>
+#include <list>
+#include <string>
+
+
+struct edf_signal_struct
+{
+	double* samples;
+	int sample_num;
+
+	edf_signal_struct() : samples(NULL), sample_num(0)
+	{}
+	~edf_signal_struct()
+	{
+		if (samples)
+			free(samples);
+	}
+};
+
+// edf_hdr_struct is defined in edflib, so here is friend operator only
+friend bool operator == (const edf_hdr_struct left, const edf_hdr_struct right)
+{
+	return (left.filetype == right.filetype) &&
+		   (left.edfsignals == right.edfsignals) &&
+		   (left.file_duration == right.file_duration) &&
+		   (left.startdate_day == right.startdate_day) &&
+		   (left.startdate_month == right.startdate_month) &&
+		   (left.startdate_year == right.startdate_year) &&
+		   (left.starttime_subsecond == right.starttime_subsecond) &&
+		   (left.starttime_second == right.starttime_second) &&
+		   (left.starttime_minute == right.starttime_minute) &&
+		   (left.starttime_hour == right.starttime_hour) &&
+		   (left.patient == right.patient) &&
+		   (left.patientcode == right.patientcode);
+}
+
+struct EDF_file
+{
+	edf_hdr_struct header;
+	std::vector<edf_annotation_struct> annotations;
+	std::vector<edf_signal_struct> signals;
+
+	friend bool operator == (const EDF_file left, const EDF_file right)
+	{
+		//return (left.header == right.header) &&
+		//	   (left.annotations == right.annotations) && 
+		//	   (left.signals == right.signals);
+
+		return (left.header == right.header);
+		// I think header determines all file
+	}
+};
+
+class edfParser
+{
+	EDF_file parseEdf(std::string path);
+	std::list<std::string> m_fnames;
+
+	public:
+
+	edfParser(std::list<std::string> file_names) : m_fnames(file_names) 
+	{}
+	std::vector<EDF_file> getData();
+};
+#endif // EDFPARSER_H
