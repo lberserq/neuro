cd1cecc4f3007fd9db9de57fd1d79f44a48935aa
[lberserq] <[berserq0123@gmail.com]>
2015-03-11 22:25:21 +0300

    fxd2

--- include/locsolvers.h
+++ include/locsolvers.h
@@ -1,10 +1,49 @@
 #ifndef LOCSOLVERS_H
 #define LOCSOLVERS_H
+#include <vector>
+#include <cmath>
 
-class locsolvers
+typedef struct s_point {
+
+    static double m_eps;
+    double coords[2];
+    friend bool operator == (const struct s_point &a, const struct s_point &b) {
+        for (int i = 0; i < 2; ++i) {
+            if ((std::abs(a.coords[i] - b.coords[i])) > m_eps) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    friend bool operator < (const s_point &a, const s_point &b) {
+        return a.coords[0] < b.coords[0] || (std::abs(a.coords[0] - b.coords[0]) < m_eps  && a.coords[1] < b.coords[1]);
+    }
+} point;
+double point::m_eps = 1e-7;
+
+
+class abstractLocSolver
 {
+protected:
+    std::vector<point> m_vct;
 public:
-    locsolvers();
+    abstractLocSolver(const std::vector<point> &vct) : m_vct(vct) {
+
+    }
+
+    virtual double getSquare() = 0;
+};
+
+
+class  naiveGrahamSolver: public abstractLocSolver
+{
+    void grahamAndrew();
+public:
+    naiveGrahamSolver(const std::vector<point> &vct) : abstractLocSolver(vct) {
+
+    }
+    double getSquare() override;
 };
 
 #endif // LOCSOLVERS_H
--- include/parsers/edfparser.h
+++ include/parsers/edfparser.h
@@ -12,62 +12,43 @@ extern "C" {
 
 struct edf_signal_struct
 {
-	double* samples;
-	int sample_num;
+    double* samples;
+    int sample_num;
 
-	edf_signal_struct() : samples(NULL), sample_num(0)
-	{}
-	~edf_signal_struct()
-	{
-		if (samples)
-			free(samples);
-	}
-    friend bool operator == (const edf_hdr_struct left, const edf_hdr_struct right);
+    edf_signal_struct() : samples(NULL), sample_num(0)
+    {}
+    ~edf_signal_struct()
+    {
+        if (samples)
+            free(samples);
+    }
 };
 
-// edf_hdr_struct is defined in edflib, so here is friend operator only
-bool operator == (const edf_hdr_struct left, const edf_hdr_struct right)
-{
-	return (left.filetype == right.filetype) &&
-		   (left.edfsignals == right.edfsignals) &&
-		   (left.file_duration == right.file_duration) &&
-		   (left.startdate_day == right.startdate_day) &&
-		   (left.startdate_month == right.startdate_month) &&
-		   (left.startdate_year == right.startdate_year) &&
-		   (left.starttime_subsecond == right.starttime_subsecond) &&
-		   (left.starttime_second == right.starttime_second) &&
-		   (left.starttime_minute == right.starttime_minute) &&
-		   (left.starttime_hour == right.starttime_hour) &&
-		   !(std::strcmp(left.patient,right.patient)) &&
-		   !(std::strcmp(left.patientcode,right.patientcode));
-}
+
+bool edfhdr_cmp(const edf_hdr_struct &left, const edf_hdr_struct &right); //magic how it works
+
 
 struct EDF_file
 {
-	edf_hdr_struct header;
-	std::vector<edf_annotation_struct> annotations;
-	std::vector<edf_signal_struct> signals;
+    edf_hdr_struct header;
+    std::vector<edf_annotation_struct> annotations;
+    std::vector<edf_signal_struct> signals;
 
-	friend bool operator == (const EDF_file left, const EDF_file right)
-	{
-		//return (left.header == right.header) &&
-		//	   (left.annotations == right.annotations) && 
-		//	   (left.signals == right.signals);
+    friend bool operator == (const EDF_file &left, const EDF_file &right)
+    {
 
-		return (left.header == right.header);
-		// I think header determines all file
-	}
+        return edfhdr_cmp(left.header, right.header);
+    }
 };
 
 class edfParser
 {
-	EDF_file parseEdf(std::string path);
-	std::list<std::string> m_fnames;
+    EDF_file parseEdf(std::string path);
+    std::list<std::string> m_fnames;
+public:
 
-	public:
-
-	edfParser(std::list<std::string> file_names) : m_fnames(file_names) 
-	{}
-	std::vector<EDF_file> getData();
+    edfParser(std::list<std::string> file_names) : m_fnames(file_names)
+    {}
+    std::vector<EDF_file> getData();
 };
 #endif // EDFPARSER_H
